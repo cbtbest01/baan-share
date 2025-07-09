d041a5eae0cc9bd5245891bb05ee6c02c78c7de6
Best Share <cbtbest01@gmail.com>
2025-07-09 08:00:35 +0700

    💄 Add home page for Baan Share

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 from flask import Flask, render_template
-import os
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
-
-if __name__ == '__main__':
-    # Port ที่ Render ให้ผ่าน environment variable PORT
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
+    return render_template('home.html', year=datetime.now().year)
